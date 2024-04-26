3632ae84f902916839adf4465ed9b2ccdfb0b433
fm_dev_mx <frgmendoza@gmail.com>
2024-04-25 17:27:05 -0700

    feat(error-handling-filter) → Implement global error handling filter
    This commit introduces a global error handling mechanism for the BuberDinner API using an `ErrorHandlingFilterAttribute`.
    
    Changes:
    
    - Modified `Program.cs` in `BuberDinner.Api`: Added `ErrorHandlingFilterAttribute` to the application pipeline to handle exceptions globally.
    - Added `ErrorHandlingFilterAttribute.cs` in `BuberDinner.Api\Filters`: A custom exception filter attribute to catch and handle exceptions, returning a standardized JSON response with a generic error message.
    
    The new error handling filter ensures that the API provides a consistent and informative response to clients, even in the event of unexpected errors, improving the overall user experience and making it easier to debug issues.

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
@@ -6,8 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-        
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
